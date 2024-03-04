8d529c3bed03b96ed5ca41f9648df02d2b84b495
Alexandre <alexandreramosjr96@gmail.com>
2024-03-04 19:58:38 -0300

    Teste

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,2 +1,5 @@
 public class Exercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -1,4 +1,4 @@
-public class Exercicio1 {
+public class Exercicio3 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
